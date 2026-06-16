c0f75893681b8e674d4c93fcded04d8c1d93e581
Andres Toro <pipetoro19@hotmail.com>
2026-06-16 16:22:24 -0500

    chore: optimize chunks splitting and increase warning threshold in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('jspdf')) return 'vendor-jspdf';
+            if (id.includes('xlsx')) return 'vendor-xlsx';
+            if (id.includes('html2canvas')) return 'vendor-html2canvas';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
